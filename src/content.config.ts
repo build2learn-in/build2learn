06a842d830742de976e2e56c771728a6a8dca5c2
Suresh Kumar <sureshdsk91@gmail.com>
2025-03-02 11:36:32 +0530

    using astro to manage content

---
+++ src/content.config.ts
@@ -0,0 +1,37 @@
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const project = defineCollection({
+	// Load Markdown and MDX files in the `src/content/project/` directory.
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		developer: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		team: z.array(z.string()).optional(),
+		tech: z.array(z.string()).optional(),
+		github_url: z.string().optional(),
+		demo_url: z.string().optional(),
+	}),
+});
+
+export const collections = { blog, project };
