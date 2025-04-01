140f9d33ca71f138dbe8671025553943a381680f
Selvakumar Duraipandian <selvasudar3@gmail.com>
2025-04-01 22:17:12 +0530

    sitemap generation test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://build2learn.in',
+  site: 'https://build2learn.pages.dev/',
   output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
   redirects: {
